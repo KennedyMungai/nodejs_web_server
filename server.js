ef716baa0e4cbda641fc0b8dfe4dc9dcf4ef0d76
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 15:30:53 +0300

    Added the dependencies for http, path, fs and fsPromises

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const logEvents = require('./logEvents')
 const EventEmitter = require('events')
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+const fsPromises = require('fs').promises
 
 class Emitter extends EventEmitter { }
 
