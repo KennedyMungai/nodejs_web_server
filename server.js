bf1ecd08707a603ba5f4db9925c72f26423980a9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 15:33:22 +0300

    Defined a server with a console log

--- server.js
+++ server.js
@@ -11,4 +11,8 @@ myEmitter = new Emitter()
 
 const PORT = process.env.PORT || 3500
 
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+})
+
 // myEmitter.on('log', (msg) => logEvents(msg))
