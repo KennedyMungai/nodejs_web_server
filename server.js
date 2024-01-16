de1a80de69362c71794a15176bb3a238dce44c32
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 15:44:40 +0300

    Removed the logic from the server js file

--- server.js
+++ server.js
@@ -13,18 +13,6 @@ const PORT = process.env.PORT || 3500
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
-
-    let filePath
-
-    if(req.url === '/' || req.url === 'index.html')
-    {
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/html')
-        filePath= path.join(__dirname, 'views', 'index.html')
-        fs.readFile(filePath,'utf-8', (err, data) => {
-            res.end(data)
-        })
-    }
 })
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
