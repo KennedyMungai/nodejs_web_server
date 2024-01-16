e1daec43d9a766aa975e2187dca0bef875e9e9fe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 15:15:20 +0300

    Added logEvents from a previos project

---
+++ logEvents.js
@@ -0,0 +1,25 @@
+const moment = require('moment')
+const { v4: uuid } = require('uuid')
+
+
+const fs = require('fs')
+const fsPromises = require('fs').promises
+const path = require('path')
+
+const logEvents = async (message) => {
+    const dateTime = `${moment(new Date()).format('LLLL')}`
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n\n`
+    console.log(logItem)
+
+    try {
+        if(!fs.existsSync(path.join(__dirname, 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, 'logs'))
+        }
+        
+        await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+module.exports = logEvents
