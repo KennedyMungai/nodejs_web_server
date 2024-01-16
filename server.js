0cf473e4ecd578d2b9511d97ee0a3eb77cab52b3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 15:57:44 +0300

    Added some logic

--- server.js
+++ server.js
@@ -42,6 +42,8 @@ const server = http.createServer((req, res) => {
     }
 
     let filePath = contentType === 'text/html' && req.url === "/" ? path.join(__dirname, 'views', 'index.html') : contentType === 'text/html' && req.url.slice(-1) === '/' ? path.join(_dirname, 'views', req.url, 'index.html') : contentType === 'text/html' ? path.join(__dirname, 'views', req.url) : path.join(__dirname, req.url)
+
+    if(!extension && req.url.slice(-1) !== '/') filePath += '.html'
 })
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
